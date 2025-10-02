e3495fcc078c4b346f5c2ea5bb3fc52d10c6cc3c
Shubhamkar Nalkar <shubhamkar.nalkar@gmail.com>
2025-10-02 22:59:03 +0530

    1212

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "901299061"
+    serviceWorkerVersion: "2341295314"
   }
 });
